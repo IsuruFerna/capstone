d4dc7faee6f0d1f70cd3102788fcd6edbfed71a5
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-06-06 09:22:24 +0200

    drop-down menu implemented using js

--- capstone/humanresources/static/humanresources/script.js
+++ capstone/humanresources/static/humanresources/script.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
   const btnDropdown = document.querySelector('.register');
   const navListSub = document.querySelector('.nav-list-sub');
+  
+  // dropdown menu: check every click event
+  document.addEventListener('click', function(event) {
+    const targetElement = event.target.parentElement;
 
-  // dropdown menu visibility
-  btnDropdown.addEventListener('click', function() {
+    if (btnDropdown.contains(targetElement)) {
+      console.log('dropdown', navListSub);
 
       // toggle visibility
       if (navListSub.classList.contains('visible')) {
+        console.log('reading hide');
         navListSub.classList.remove('visible');
         navListSub.classList.add('invisible');
       } else {
+        console.log('reading add');
         navListSub.classList.remove('invisible');
         navListSub.classList.add('visible');
       }
+    } else {
+
+      // if user click out of dropdown menu, hide it
+      navListSub.classList.remove('visible');
+      navListSub.classList.add('invisible');
+    }
   })
+
 });
