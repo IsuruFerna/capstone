767b78a55e85999dc76dd741c65c2cb776706b2a
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-06-06 10:49:38 +0200

    User model created and bug fixed

--- capstone/humanresources/models.py
+++ capstone/humanresources/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
-
-
 class User(AbstractUser):
     # pass
+    MAIN = '1'
+    EMPLOYER = '2'
+    EMPLOYEE = '3'
+    ACCOUNT_TYPE_CHOICES = [
+        (MAIN, "Main"),
+        (EMPLOYER, "Employer"),
+        (EMPLOYEE, "Employee")
+    ]
+
+    account_type = models.CharField(max_length=8, blank=False,
+                                    null=False, choices=ACCOUNT_TYPE_CHOICES, default=EMPLOYEE)
     password_reset = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.username}, password reseted: {self.password_reset}"
-
-    class Meta:
-        abstract = True
+        return f"Account Type:{self.account_type}: {self.username}, password reseted: {self.password_reset}"
